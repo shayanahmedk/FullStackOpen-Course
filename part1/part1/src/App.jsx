4446bb2fe1fd5eea18e244ff006243b651d643ed
shayaaan <42377211+shayanahmedk@users.noreply.github.com>
2024-01-26 19:05:35 +0500

    created new app and edited code of it for exercise 1.1 and 1.2

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -45,24 +45,48 @@
 //
 // export default App
 
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
+// const Hello = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <p>Hello {props.name}, you are {props.age} years old</p>
+//     </div>
+//   )
+// }
+
+// const Footer = () => {
+//   return (
+//     <div>
+//       greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name='Shayan'
+//   const age=22
+
+//   return (
+//     // <div>
+//     <>
+//       <h1>Greetings</h1>
+//       <Hello name='Maya' age={26+10} />
+//       <Hello name={name} age={age} />
+//       <Footer />
+//       </>
+//     // </div>
+//   )
+// }
+
+// export default App
+
 
 const App = () => {
-  const name='Shayan'
-  const age=22
+  const friends = [ 'Peter', 'Maya']
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26+10} />
-      <Hello name={name} age={age} />
+      <p>{friends}</p>
     </div>
   )
 }
