5548d7bbe1679a2702be928009719e954763819d
shayaaan <42377211+shayanahmedk@users.noreply.github.com>
2024-01-30 03:20:01 +0500

    edited App.jsx and main.jsx

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,99 +1,43 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import { useState } from 'react'
 
-// function App() {
-//   const [count, setCount] = useState(0)
+const Display = ({counter}) => <div>{counter}</div>
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
-const Part = (props) => {
+const Button = ({onClick, text}) => {
   return (
-    <div>
-      <p>{props.name}: {props.exercises}</p>
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
-      <Part name={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
-      <Part name={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercises: {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
-      </p>
-    </div>
+    <button onClick={onClick}>
+      {text}
+    </button>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [counter, setCounter] = useState(0)
+  console.log('renderring with counter value...', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before...', counter)
+    setCounter(counter+1)
+  }
+  const decreaseByone = () => {
+    console.log('decreasing, value before...', counter)
+    setCounter(counter-1)
+  }
+  const setToZero = () => {
+    console.log('resetting value to zero, value before...', counter)
+    setCounter(0)
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+    <Display counter={counter} />
+    <br />
+    <Button onClick={increaseByOne} text='plus' />
+    <br />
+    <br />
+    <Button onClick={setToZero} text='zero' />
+    <br />
+    <br />
+    <Button onClick={decreaseByone} text='minus' />
     </div>
   )
 }
